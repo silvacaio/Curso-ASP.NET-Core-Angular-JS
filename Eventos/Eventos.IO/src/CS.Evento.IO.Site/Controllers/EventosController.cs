a97665f0c683abba518bb3064c8891e87a50a7d5
silvacaio <silva.caiosfc@hotmail.com>
2018-09-01 17:06:57 -0300

    Iniciada a camada de apresentação - Injeção de dependencia

---
+++ Eventos/Eventos.IO/src/CS.Evento.IO.Site/Controllers/EventosController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CS.Evento.IO.Site.Data;
+using CS.Eventos.IO.Application.ViewModels;
+using CS.Eventos.IO.Application.Interfaces;
+
+namespace CS.Evento.IO.Site.Controllers
+{
+    public class EventosController : Controller
+    {
+        private readonly IEventoAppService _eventoAppService;
+
+        public EventosController(IEventoAppService eventoAppService)
+        {
+            _eventoAppService = eventoAppService;
+        }
+
+        // GET: Eventos
+        public IActionResult Index()
+        {
+            return View(_eventoAppService.ObterTodos());
+        }
+
+        // GET: Eventos/Details/5
+        public IActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventoViewModel = _eventoAppService.ObertPorId(id.Value);
+
+            if (eventoViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventoViewModel);
+        }
+
+        // GET: Eventos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Nome,DescricaoCurta,DescricaoLonga,DataInicio,DateFinal,Gratuito,Valor,Online,NomeEmpresa,CategoriaId,OrganizadorId")] EventoViewModel eventoViewModel)
+        {
+            if (!ModelState.IsValid) return View(eventoViewModel);
+
+            _eventoAppService.Registrar(eventoViewModel);
+
+            return View(eventoViewModel);
+        }
+
+        // GET: Eventos/Edit/5
+        public IActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventoViewModel = _eventoAppService.ObertPorId(id.Value);
+            if (eventoViewModel == null)
+            {
+                return NotFound();
+            }
+            
+            return View(eventoViewModel);
+        }
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind("Id,Nome,DescricaoCurta,DescricaoLonga,DataInicio,DateFinal,Gratuito,Valor,Online,NomeEmpresa,CategoriaId,OrganizadorId")] EventoViewModel eventoViewModel)
+        {
+            if (!ModelState.IsValid) return View(eventoViewModel);
+
+            _eventoAppService.Atualizar(eventoViewModel);
+
+            //TODO: validar se a operação ocorreu com suceso
+
+            return View(eventoViewModel);
+        }
+
+        // GET: Eventos/Delete/5
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventoViewModel = _eventoAppService.ObertPorId(id.Value);
+            if (eventoViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventoViewModel);
+        }
+
+        // POST: Eventos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(Guid id)
+        {
+            _eventoAppService.Excluir(id);
+            return RedirectToAction(nameof(Index));
+        }       
+    }
+}
---
+++ Eventos/Eventos.IO/src/CS.Eventos.IO.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using CS.Eventos.IO.Application.Interfaces;
+using CS.Eventos.IO.Application.Services;
+using CS.Eventos.IO.Domain.Core.Bus;
+using CS.Eventos.IO.Domain.Core.Events;
+using CS.Eventos.IO.Domain.Core.Notifications;
+using CS.Eventos.IO.Domain.Eventos.Commands;
+using CS.Eventos.IO.Domain.Eventos.Events;
+using CS.Eventos.IO.Domain.Eventos.Repository;
+using CS.Eventos.IO.Domain.Interfaces;
+using CS.Eventos.IO.Infra.Data.Context;
+using CS.Eventos.IO.Infra.Data.Repository;
+using CS.Eventos.IO.Infra.Data.UoW;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CS.Eventos.IO.Infra.CrossCutting.IoC
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application
+            services.AddScoped<IEventoAppService, EventoAppService>();
+            services.AddSingleton(Mapper.Configuration);
+            services.AddScoped<IMapper>(sp => new Mapper(sp.GetRequiredService<IConfigurationProvider>(), sp.GetServices));
+
+            //Domain - Commands
+            services.AddScoped<IHandler<RegistrarEventoCommand>, EventoCommandHandler>();
+            services.AddScoped<IHandler<AtualizarEventoCommand>, EventoCommandHandler>();
+            services.AddScoped<IHandler<RemoverEventoCommand>, EventoCommandHandler>();
+
+            //Domain - Events
+            services.AddScoped<IDomainNotificationHandler<DomainNotification>, DomainNotificationHandler>();
+            services.AddScoped<IHandler<EventoRegistradoEvent>, EventoEventHandler>();
+            services.AddScoped<IHandler<EventoAtualizadoEvent>, EventoEventHandler>();
+            services.AddScoped<IHandler<EventoRemovidoEvent>, EventoEventHandler>();
+
+            //Infra - Data
+            services.AddScoped<IEventoRepository, EventoRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<EventosContext>();
+
+            //Infra - Bus
+            //services.AddScoped<IBus, InMemoryBus>();
+        }
+    }
+}

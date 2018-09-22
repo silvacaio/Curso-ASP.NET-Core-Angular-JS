0bb1e9520319547e064513faadde6fa76427be8b
silvacaio <silva.caiosfc@hotmail.com>
2018-09-22 14:26:41 -0300

    Iniciado o projeto angular

---
+++ Eventos/client/Eventos/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CollapseModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Eventos/client/Eventos/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  envName: 'dev' //para fazer injeção de dependencia em components
+};
